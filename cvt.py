76ecbc59fddb1b7f9b226686075be241fe849b05
bossdm <davidmbossens@gmail.com>
2019-05-10 13:09:44 +0100

    correction for relative directory if args.f=""

--- cvt.py
+++ cvt.py
@@ -31,7 +31,7 @@ X = np.random.rand(NumberOfSampedPoints,DIM)
 k_means = KMeans(init='k-means++', n_clusters=k, n_init=1, n_jobs=-1, verbose=1)
 k_means.fit(X)
 k_means_cluster_centers = k_means.cluster_centers_
-
+args.f = args.f if args.f=="" else args.f+"/"
 filename = args.f+'/centroids_' + str(k) + '_' + str(DIM) + '%s.dat'%(args.t)
 
 with open(filename, 'w') as f:
