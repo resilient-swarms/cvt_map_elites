7f8eee8459d612496f2e1f077e3cd36e3c27d80d
bossdm <bossdm@github.com>
2019-08-04 04:29:00 +0100

    allow batch-based KMeans

--- cvt.py
+++ cvt.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans,MiniBatchKMeans
 
 import argparse
+import pickle
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 parser.add_argument('-k', type=int,default=1000,
@@ -18,6 +19,12 @@ parser.add_argument('-s', type=int,default=1,
                     help='seed number')
 parser.add_argument('-t', type=str,default="",
                     help='a tag to help identify (e.g. run number)')
+parser.add_argument('-j', type=int,default=-1,
+                    help='the number of parallel jobs for KMeans')
+parser.add_argument('-l', type=str,default="",
+                    help='prefix to batch files')
+parser.add_argument('-b', type=int ,default=0,
+                    help='batch size for batch-based k-means')
 args = parser.parse_args()
 
 k = args.k
@@ -28,8 +35,16 @@ np.random.seed(args.s)
 
 X = np.random.rand(NumberOfSampedPoints,DIM)
 
-k_means = KMeans(init='k-means++', n_clusters=k, n_init=1, n_jobs=-1, verbose=1)
-k_means.fit(X)
+if args.b > 0:
+    k_means = MiniBatchKMeans(init='k-means++', n_clusters=k, n_init=1, verbose=1,batch_size=args.b)
+    j=0
+    for i in range(0,args.p,args.b):
+        X = pickle.load(open(args.l+str(j),"rb"))
+        k_means.partial_fit(X)
+        j+=1
+else:
+    k_means = KMeans(init='k-means++', n_clusters=k, n_init=1, n_jobs=args.j, verbose=1)
+    k_means.fit(X)
 k_means_cluster_centers = k_means.cluster_centers_
 args.f = args.f if args.f=="" else args.f+"/"
 filename = args.f+'centroids_' + str(k) + '_' + str(DIM) + '%s.dat'%(args.t)
